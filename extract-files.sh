d3cb9dac38da020eec2b94aa0fa87ac327ad04eb
Simon1511 <me@simon1511.de>
2024-01-05 17:20:59 +0100

    gta4xl: libsec-ril: Always return an empty value for GET_SMSC_ADDRESS
    * Make parameter of type RilData (second last one) passed to SecRil::RequestComplete()
      in OnGetSmscAddressDone() be NULL, so that the framework passes an empty value when sending SMS
    * Before U this value was not being used, on U the value returned is "SMSC",format_specifier
      which is considered invalid when passed back to RIL for sending SMS, however when an empty value
      is passed RIL will select the default SMSC, like it did on T and sending SMS works again
    
    Change-Id: I608be406969d7fa58f15ef36340aff4ad1ce9fd0
    Co-authored-by: Tim Zimmermann <tim@linux4.de>

--- extract-files.sh
+++ extract-files.sh
@@ -11,8 +11,13 @@ function blob_fixup() {
         vendor/lib/hw/audio.primary.exynos9611.so)
             "${PATCHELF}" --remove-needed libaudio_soundtrigger.so "${2}"
             ;;
-        vendor/bin/hw/rild|vendor/lib*/libsec-ril.so)
+        vendor/bin/hw/rild)
             "${PATCHELF}" --replace-needed libril.so libril-samsung.so "${2}"
+            ;;
+        vendor/lib*/libsec-ril.so)
+            "${PATCHELF}" --replace-needed libril.so libril-samsung.so "${2}"
+            xxd -p -c0 "${2}" | sed "s/600e40f9820c805224008052e10315aae30314aa/600e40f9820c805224008052e10315aa030080d2/g" | xxd -r -p > "${2}".patched
+            mv "${2}".patched "${2}"
             ;;
     esac
 }
